e419c3361bd3f2fa92c4ec6b95b9d31666138ccd
Tim H. <tim@heidelba.ch>
2023-06-15 11:16:39 +0200

    Fix deprecation in URL api

--- rcp.cli/src/main/java/dev/tim9h/rcp/cli/query/QueryService.java
+++ rcp.cli/src/main/java/dev/tim9h/rcp/cli/query/QueryService.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -53,7 +54,7 @@ public class QueryService {
 				new InputStreamReader(getConnection(URL_INSTANT_ANSWER, query).getInputStream()))) {
 			return gson.fromJson(in, InstantAnswer.class);
 
-		} catch (IOException | JsonIOException | JsonSyntaxException e) {
+		} catch (IOException | JsonIOException | JsonSyntaxException | URISyntaxException e) {
 			logger.error(() -> "Unable to get instant answer", e);
 			return null;
 		}
@@ -65,14 +66,15 @@ public class QueryService {
 			List<Suggestion> suggestions = gson.fromJson(in, TYPE_SUGGESTIONLIST);
 			return suggestions.stream().map(Suggestion::getPhrase).toList();
 
-		} catch (IOException e) {
+		} catch (IOException | URISyntaxException e) {
 			logger.error(() -> "Unable to get suggestions", e);
 			return Collections.emptyList();
 		}
 	}
 
-	private static HttpsURLConnection getConnection(String urlString, String query) throws IOException {
-		var url = new URL(String.format(urlString, URLEncoder.encode(query, StandardCharsets.UTF_8)));
+	private static HttpsURLConnection getConnection(String urlString, String query)
+			throws IOException, URISyntaxException {
+		var url = new URI(String.format(urlString, URLEncoder.encode(query, StandardCharsets.UTF_8))).toURL();
 		var con = (HttpsURLConnection) url.openConnection();
 		con.setRequestMethod(METHOD_GET);
 		con.setRequestProperty(USER_AGENT, USER_AGENT_VALUE);
